5051f3662f3ca326ba4e011e9ffd660202ca2825
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-20 16:32:33 +0300

    Made a singleton for context.

---
+++ DAL/Tools/DbContextSingleton.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Tools
+{
+    public class DbContextSingleton 
+    {
+        private DbContextSingleton() //Singleton
+        {
+
+        }
+
+        private static ECommerceContext _context; 
+        public static ECommerceContext Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = new ECommerceContext();
+                }
+                return _context;
+            }
+        }
+    }
+}
