b008d8a6705cb2b201370bb8678f1d92770d27ad
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-21 19:24:26 +0300

    Login has been done.

---
+++ MVC_UI/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using BLL.Concrete;
+using DAL.Entity;
+using MVC_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        AppUserService appUserService = new AppUserService();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginUserVM loginUserVM)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = appUserService.Any(x => x.UserName == loginUserVM.UserName && x.Password == loginUserVM.Password);
+                if (result)
+                {
+                    AppUser user = appUserService.GetDefault(x => x.UserName == loginUserVM.UserName).FirstOrDefault();
+                    FormsAuthentication.SetAuthCookie(user.UserName, true); //create cookies
+                    Session["login"] = user;
+                    if (user.UserName == "enes")
+                    {
+                        return RedirectToAction("Index", "Admin/Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    TempData["error"] = $"Username/Password is incorrect. Please check your information..";
+                    return View(loginUserVM);
+                }
+            }
+            else
+            {
+                return View(loginUserVM);
+            }
+        }
+    }
+}
---
+++ MVC_UI/Models/LoginUserVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_UI.Models
+{
+    public class LoginUserVM
+    {
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        public string Password { get; set; }
+    }
+}
