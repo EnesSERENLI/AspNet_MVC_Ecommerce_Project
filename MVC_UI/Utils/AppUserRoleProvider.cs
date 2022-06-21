af8730e8807239665bd73d218ebd2cc858829232
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-21 19:32:28 +0300

    Authorization and orientation done.

---
+++ MVC_UI/Utils/AppUserRoleProvider.cs
@@ -0,0 +1,67 @@
+﻿using DAL.Context;
+using DAL.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MVC_UI.Utils
+{
+    public class AppUserRoleProvider : RoleProvider
+    {
+        ECommerceContext db = DbContextSingleton.Context;
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username) //Return the user's role as an array.
+        {
+            var userRoleList = from user in db.AppUsers join userandrole in db.AppUserAndRoles on user.ID equals userandrole.AppUserId join role in db.AppUserRoles on userandrole.AppUserRoleId equals role.ID where user.UserName == username select role.RoleName;
+            return userRoleList.ToArray();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
